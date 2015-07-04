855a9befc23f6d9adab134d08def932db5e4ccaf
yoichigmf <yoichi.kayama@gmail.com>
2015-07-04 11:03:23 +0900

    間違い修正

--- cesium-starter-app_Iknowshachu/Source/App.js
+++ cesium-starter-app_Iknowshachu/Source/App.js
@@ -1,3 +1,3 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
-viewer.cameraectangle(Cesium.Rectangle.fromDegrees(114.591, -45.837, 148.970, -5.730));
+viewer.camera.viewRectangle(Cesium.Rectangle.fromDegrees(114.591, -45.837, 148.970, -5.730));
