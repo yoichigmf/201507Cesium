c0f3de5a7d70c02755ac220c93498fa87626f449
yoichigmf <yoichi.kayama@gmail.com>
2015-07-04 11:38:47 +0900

    Spell miss

--- cesium-starter-app_Iknowshachu/Source/App.js
+++ cesium-starter-app_Iknowshachu/Source/App.js
@@ -1,4 +1,4 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
-viewer.dataSource.add(Cesium.KmlDataSource.load('http://yoichigmf.github.io/201507Cesium/cesium-starter-app_Iknowshachu/Source/20150703.kml'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('http://yoichigmf.github.io/201507Cesium/cesium-starter-app_Iknowshachu/Source/20150703.kml'));
 
